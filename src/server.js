64d39bd19c9017d6c1022ab680b7a844fa6887bb
tamzid49 <46148776+tamzid49@users.noreply.github.com>
2021-08-11 11:18:05 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const colyseus_1 = require("colyseus");
 const deatchmatch_1 = require("./Rooms/deatchmatch");
-const port = 3000;
+const port = process.env.PORT || 3000
 const gameServer = new colyseus_1.Server();
 gameServer.define("deathmatch", deatchmatch_1.DeathMatch);
 gameServer.listen(port);
